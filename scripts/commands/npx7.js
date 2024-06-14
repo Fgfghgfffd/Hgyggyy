5b123e0e3e2e0ea009e428e103059c64b1409be1
Botvai12 <vaibot932@gmail.com>
2024-06-15 00:02:30 +0300

    Update npx7.js

--- scripts/commands/npx7.js
+++ scripts/commands/npx7.js
@@ -19,14 +19,14 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/pXaA17P.mp4',
+      'https://i.imgur.com/ijiP1I9.mp4',
       { responseType: 'stream' }
     )
   ).data;
 
-	if (body.indexOf("fider")==0 || body.indexOf("dud")==0 || body.indexOf("dudu")==0 || body.indexOf("🍼")==0 || body.indexOf("pider")==0 || body.indexOf("pm pm")==0 || body.indexOf("pom pom")==0 || body.indexOf("pm pm khamu")==0 || body.indexOf("fider")==0 || body.indexOf("milk")==0) {
+	if (body.indexOf("kosto")==0 || body.indexOf("taka")==0 || body.indexOf("ke")==0 || body.indexOf("😱")==0 || body.indexOf("🤧")==0 || body.indexOf("prem")==0 || body.indexOf("prem")==0 || body.indexOf("chipai")==0 || body.indexOf("chipay")==0 || body.indexOf("milk")==0) {
 		var msg = {
-				body: "𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "❤️‍🔥💥 FARABI 💥❤️‍🔥",
 				attachment: media
 			}
 			api.sendMessage( msg, threadID, messageID);
