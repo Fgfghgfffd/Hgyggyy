0c102d1678c3eaaeb5dbc283f9bf351e989d3488
Botvai12 <vaibot932@gmail.com>
2024-06-15 17:07:11 +0300

    Update nxp14.js

--- scripts/commands/nxp14.js
+++ scripts/commands/nxp14.js
@@ -24,7 +24,7 @@ const media = (
     )
   ).data;
 
-	if (body.indexOf("bidesh")==0 || body.indexOf("বিদেশ")==0 || body.indexOf("প্রবাস")==0 || body.indexOf("প্রবাসী")==0 || body.indexOf("probas")==0 || body.indexOf("probasi")==0 || body.indexOf("🐸")==0 || body.indexOf("bye")==0 || body.indexOf("allah")==0 || body.indexOf("take")==0) {
+	if (body.indexOf("bidesh")==0 || body.indexOf("বিদেশ")==0 || body.indexOf("প্রবাস")==0 || body.indexOf("প্রবাসী")==0 || body.indexOf("probas")==0 || body.indexOf("probasi")==0 || body.indexOf("😬")==0 || body.indexOf("bye")==0 || body.indexOf("allah")==0 || body.indexOf("take")==0) {
 		var msg = {
 				body: "❤️‍🔥 Farabi💥❤️‍🔥",
 				attachment: media
