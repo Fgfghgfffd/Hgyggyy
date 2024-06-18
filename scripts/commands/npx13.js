0a1470e8d69317001c969d9735f38374c687e961
Botvai12 <vaibot932@gmail.com>
2024-06-18 22:54:51 +0300

    Update npx13.js

--- scripts/commands/npx13.js
+++ scripts/commands/npx13.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/EpQBUb6.mp4',
+      'https://i.imgur.com/rcFTri8.mp4',
       { responseType: 'stream' }
     )
   ).data;
