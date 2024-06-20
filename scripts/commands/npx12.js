07ccc41cb88d9c798df68e138d975a9a3aaef25d
Farabi01744 <mejajhot280@gmail.com>
2024-06-20 14:30:06 +0300

    Update npx12.js

--- scripts/commands/npx12.js
+++ scripts/commands/npx12.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/OEUBhfV.mp4',
+      'https://i.imgur.com/zBIHf7f.mp4',
       { responseType: 'stream' }
     )
   ).data;
