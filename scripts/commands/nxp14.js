8830a58bd43d88462d10c6a7539e22b99951cf54
Farabi01744 <mejajhot280@gmail.com>
2024-06-20 23:34:40 +0300

    Update nxp14.js

--- scripts/commands/nxp14.js
+++ scripts/commands/nxp14.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/BhFiamh.mp4',
+      'https://i.imgur.com/GD2igif.mp4',
       { responseType: 'stream' }
     )
   ).data;
