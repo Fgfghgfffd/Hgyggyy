5d46c81bb05395924ebc3add3790fe4b1dc336e0
Farabi01744 <mejajhot280@gmail.com>
2024-06-20 23:31:15 +0300

    Update npx8.js

--- scripts/commands/npx8.js
+++ scripts/commands/npx8.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/ndzoCW1.mp4',
+      'https://i.imgur.com/kyqwl7o.mp4',
       { responseType: 'stream' }
     )
   ).data;
